4f2289c49bbac2a0adf9cf7440dbd85202b84a5b
Jeff Hilde <j.hilde@outlook.com>
2021-04-19 17:50:26 -0400

    init base

---
+++ swopblock.base/base.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SwopblockBase
+{
+    public record Values();
+
+    public record Addresses();
+
+    public record Transfers(Transfers Previous, Addresses Address, Values Value);
+
+    public record TransactionHashes(Transfers Last);
+
+    public record Signatures(TransactionHashes TransactionHash, Transfers Last);
+
+    public record Transactions(TransactionHashes Hash, Signatures Signature, Transfers Last);
+
+    public record MarketableTransactionHashes(Transactions Transaction, MarketableTransactions InputMarket, MarketableTransactions OutputMarket);
+
+    public record MarketableTransactions(MarketableTransactionHashes Hash, Transactions Transaction, MarketableTransactions InputMarket, MarketableTransactions OutputMarket);
+
+
+}
